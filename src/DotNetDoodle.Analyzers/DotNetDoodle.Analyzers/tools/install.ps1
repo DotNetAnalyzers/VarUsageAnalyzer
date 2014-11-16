01f32776914e046eced4ad7146dd2a4415e3be4d
tugberkugurlu <tugberkugurlu@gmail.com>
2014-11-16 13:18:41 +0200

    initial load with empty project template

---
+++ src/DotNetDoodle.Analyzers/DotNetDoodle.Analyzers/tools/install.ps1
@@ -0,0 +1,6 @@
+﻿param($installPath, $toolsPath, $package, $project)
+
+$analyzerPath = join-path $toolsPath "analyzers"
+$analyzerFilePath = join-path $analyzerPath "DotNetDoodle.Analyzers.dll"
+
+$project.Object.AnalyzerReferences.Add("$analyzerFilePath")
